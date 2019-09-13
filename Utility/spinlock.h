9718867bf1fe4bde826b72543b21b0684f25e989
mengps <843261040@qq.com>
2019-09-13 23:51:06 +0800

    添加实用工具SpinLock

---
+++ Utility/spinlock.h
@@ -0,0 +1,25 @@
+#ifndef SPINLOCK_H
+#define SPINLOCK_H
+
+#include <atomic>
+
+class SpinLock
+{
+public:
+    void lock() {
+        while (isLocked());
+    }
+
+    void unlock() {
+        m_lock.clear();
+    }
+
+private:
+    bool isLocked() {
+        return m_lock.test_and_set();
+    }
+
+    std::atomic_flag m_lock = ATOMIC_FLAG_INIT;
+};
+
+#endif
